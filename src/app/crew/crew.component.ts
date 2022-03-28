12b06be21a81b510c9e92bda5c4c46857dea5fb1
Victoria <weddev2021vg@gmail.com>
2022-03-28 12:26:23 -0500

    part2

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -3,19 +3,45 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-crew',
   templateUrl: './crew.component.html',
-  styleUrls: ['./crew.component.css']
+  styleUrls: ['./crew.component.css'],
 })
 export class CrewComponent implements OnInit {
+  memberBeingEdited: object = null;
+  inCrew: boolean = false;
 
   crew: object[] = [
-    {name: "Eileen Collins", firstMission: false},
-    {name: "Mae Jemison", firstMission: false},
-    {name: "Ellen Ochoa", firstMission: true}
+    { name: 'Eileen Collins', firstMission: false },
+    { name: 'Mae Jemison', firstMission: false },
+    { name: 'Ellen Ochoa', firstMission: true },
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  add(memberName: string, isFirst: boolean) {
+    for (let i = 0; i < this.crew.length; i++) {
+      if (this.crew[i]['name'] === memberName) {
+        this.inCrew = true;
+      }
+    }
+    if (!this.inCrew) {
+      this.crew.push({ name: memberName, firstMission: isFirst });
+    }
+    this.inCrew = false;
   }
 
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+
+  edit(member: object) {
+    this.memberBeingEdited = member;
+  }
+
+  save(name: string, member: object) {
+    member['name'] = name;
+    this.memberBeingEdited = null;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,22 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-
-  missionName: string = "Mars 2030";
-  rocketName: string = "Plasma Max";
+  missionName: string = 'Apollo 11';
+  rocketName: string = 'Saturn V';
+  editingRocket: boolean = false;
   editingMission: boolean = false;
-  
-  constructor() { }
 
-  ngOnInit() {
-  }
+  constructor() {}
+
+  ngOnInit() {}
 
   updateMission(updatedName: string) {
     this.missionName = updatedName;
     this.editingMission = false;
   }
 
+  updateRocket(updatedName: string) {
+    this.rocketName = updatedName;
+    this.editingRocket = false;
+  }
 }
