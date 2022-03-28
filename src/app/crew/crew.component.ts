a9c7c2dc5fb82065ae761d694a4c08ffc55e98f5
Victoria <weddev2021vg@gmail.com>
2022-03-28 12:46:38 -0500

    part3

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -3,27 +3,66 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-crew',
   templateUrl: './crew.component.html',
-  styleUrls: ['./crew.component.css']
+  styleUrls: ['./crew.component.css'],
 })
 export class CrewComponent implements OnInit {
-
   inCrew: boolean = false;
   crew: object[] = [];
 
   candidates: object[] = [
-    {name: "Sally Ride", photo: 'https://handlers.education.launchcode.org/static/images/sally-ride.jpg'},
-    {name: "Mae Jemison", photo: 'https://handlers.education.launchcode.org/static/images/mae-jemison.jpg'},
-    {name: "Ellen Ochoa", photo: 'https://handlers.education.launchcode.org/static/images/ellen-ochoa.jpg'},
-    {name: "Frederick Gregory", photo: 'https://handlers.education.launchcode.org/static/images/frederick-gregory.jpg'},
-    {name: "Guion Bluford", photo: 'https://handlers.education.launchcode.org/static/images/guion-bluford.jpg'},
-    {name: "Kjell Lindgren", photo: 'https://handlers.education.launchcode.org/static/images/kjell-lindgren.jpg'},
-    {name: "Jeanette Epps", photo: 'https://handlers.education.launchcode.org/static/images/jeanette-epps.jpg'}
+    {
+      name: 'Sally Ride',
+      photo:
+        'https://handlers.education.launchcode.org/static/images/sally-ride.jpg',
+    },
+    {
+      name: 'Mae Jemison',
+      photo:
+        'https://handlers.education.launchcode.org/static/images/mae-jemison.jpg',
+    },
+    {
+      name: 'Ellen Ochoa',
+      photo:
+        'https://handlers.education.launchcode.org/static/images/ellen-ochoa.jpg',
+    },
+    {
+      name: 'Frederick Gregory',
+      photo:
+        'https://handlers.education.launchcode.org/static/images/frederick-gregory.jpg',
+    },
+    {
+      name: 'Guion Bluford',
+      photo:
+        'https://handlers.education.launchcode.org/static/images/guion-bluford.jpg',
+    },
+    {
+      name: 'Kjell Lindgren',
+      photo:
+        'https://handlers.education.launchcode.org/static/images/kjell-lindgren.jpg',
+    },
+    {
+      name: 'Jeanette Epps',
+      photo:
+        'https://handlers.education.launchcode.org/static/images/jeanette-epps.jpg',
+    },
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() { }
+  ngOnInit() {}
 
-  // Code the 'addCrewMember' function here:
+  addCrewMember(member: object) {
+    if (this.crew.includes(member)) {
+      this.inCrew = true;
+    } else {
+      this.inCrew = false;
+    }
 
+    if (this.inCrew) {
+      let index = this.crew.indexOf(member);
+      this.crew.splice(index, 1);
+    } else if (!this.inCrew && this.crew.length < 3) {
+      this.crew.push(member);
+    }
+  }
 }
